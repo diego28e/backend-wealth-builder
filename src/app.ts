726d9fa629854a89b5d7ff027dbba0ebee9a7639
Diego E <diego28e@gmail.com>
2025-11-16 12:43:07 -0500

    feat:added urlencoded to the app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import express from "express";
+import type {Application, Request, Response} from "express";
 import router from "./routes/index.js";
-const app = express();
+const app: Application= express();
 app.use(express.json());
+
+app.get('/', (req:Request, res:Response)=>{
+    res.status(200).send("Toon Wealth API is running")
+})
+
+//URL-encoded parser
+app.use(express.urlencoded({extended:true}))
 
 app.use('/api', router)
 
