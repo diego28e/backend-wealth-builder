de6e81ffbcc956c443e9ed345c209569f9c4cb6e
Diego E <diego28e@gmail.com>
2026-02-06 09:34:36 -0500

    feat:implemented cors

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 import express from "express";
-import type {Application, Request, Response} from "express";
+import type { Application, Request, Response } from "express";
+import cors from 'cors';
 import router from "./routes/index.js";
 
 const app: Application = express();
+
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true
+}))
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.get('/', (req: Request, res: Response) => {
-    res.status(200).send("Toon Wealth API is running")
+  res.status(200).send("Toon Wealth API is running")
 })
 
 app.use('/api/v1', router)
